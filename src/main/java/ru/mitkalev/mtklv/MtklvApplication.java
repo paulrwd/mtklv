fa9168c0bb9290311fcb0bc2a436a57fd9b64a29
Павел Миткалёв <p.mitkalev@r77.center-inform.ru>
2024-11-14 15:43:12 +0300

    init

---
+++ src/main/java/ru/mitkalev/mtklv/MtklvApplication.java
@@ -0,0 +1,13 @@
+package ru.mitkalev.mtklv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtklvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtklvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mitkalev/mtklv/MtklvApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mitkalev.mtklv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtklvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
