d022d62f48362f174bd1b03d00cc03f88e19a7bb
Павел Миткалёв <p.mitkalev@r77.center-inform.ru>
2024-11-15 12:03:18 +0300

    add FirstService

--- src/main/java/ru/mitkalev/mtklv/MtklvApplication.java
+++ src/main/java/ru/mitkalev/mtklv/MtklvApplication.java
@@ -2,12 +2,12 @@ package ru.mitkalev.mtklv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MtklvApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MtklvApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MtklvApplication.class, args);
+    }
 }
