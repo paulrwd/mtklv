d59432b3f3840981b15c70540ac84c544154aafe
Павел Миткалёв <p.mitkalev@r77.center-inform.ru>
2024-12-09 10:43:58 +0300

    add SecondService

---
+++ src/main/java/ru/mitkalev/mtklv/service/ThirdService.java
@@ -0,0 +1,21 @@
+package ru.mitkalev.mtklv.service;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Data
+public class ThirdService {
+    @Value("${app.value}")
+    private String val;
+    @Scheduled(fixedDelayString = "${app.scheduled.delay.milliseconds}")
+    public void process() {
+        log.info(val);
+    }
+}
